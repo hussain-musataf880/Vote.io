735aaa093e7edff3b3bc688e91824aed532ac1c2
Akshit Anand <aa4253@drexel.edu>
2019-05-04 20:47:44 -0400

    Changes

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -71,7 +71,119 @@ class HomePage extends StatelessWidget {
 
 
                         )])
-                  )],
+                  ),
+                  Container(
+
+                      padding: EdgeInsets.symmetric(horizontal:20.0, vertical: 10.0),
+                      child:Stack(
+                          children: <Widget> [
+                            Image.asset("assets/gen_person.png", height: 180.0, width: 110.0),
+                            Container(
+                                padding: EdgeInsets.fromLTRB(200.0,40.0,10.0,10.0),
+                                child: Stack(
+                                    children: <Widget>[
+                                      Stack(
+                                        children: <Widget>[
+                                          Column(
+
+                                            children: <Widget>[
+                                              Container(
+                                                  padding: EdgeInsets.fromLTRB(5,40,5,5),
+                                                  child:Text("Name",style: TextStyle(fontWeight: FontWeight.bold))
+                                              ),
+                                              SizedBox(height: 2.0,),
+                                              Container(
+                                                  child:Text("Decription")
+                                              ),
+                                              SizedBox(height: 2.0,),
+                                              Container(
+                                                  child:Text("Location")
+                                              )
+                                            ],
+
+                                          )
+                                        ],
+                                      )
+                                    ])
+
+
+                            )])
+                  ),
+                  Container(
+
+                      padding: EdgeInsets.symmetric(horizontal:20.0, vertical: 10.0),
+                      child:Stack(
+                          children: <Widget> [
+                            Image.asset("assets/gen_person.png", height: 180.0, width: 110.0),
+                            Container(
+                                padding: EdgeInsets.fromLTRB(200.0,40.0,10.0,10.0),
+                                child: Stack(
+                                    children: <Widget>[
+                                      Stack(
+                                        children: <Widget>[
+                                          Column(
+
+                                            children: <Widget>[
+                                              Container(
+                                                  padding: EdgeInsets.fromLTRB(5,40,5,5),
+                                                  child:Text("Name",style: TextStyle(fontWeight: FontWeight.bold))
+                                              ),
+                                              SizedBox(height: 2.0,),
+                                              Container(
+                                                  child:Text("Decription")
+                                              ),
+                                              SizedBox(height: 2.0,),
+                                              Container(
+                                                  child:Text("Location")
+                                              )
+                                            ],
+
+                                          )
+                                        ],
+                                      )
+                                    ])
+
+
+                            )])
+                  ),
+                  Container(
+
+                      padding: EdgeInsets.symmetric(horizontal:20.0, vertical: 10.0),
+                      child:Stack(
+                          children: <Widget> [
+                            Image.asset("assets/gen_person.png", height: 180.0, width: 110.0),
+                            Container(
+                                padding: EdgeInsets.fromLTRB(200.0,40.0,10.0,10.0),
+                                child: Stack(
+                                    children: <Widget>[
+                                      Stack(
+                                        children: <Widget>[
+                                          Column(
+
+                                            children: <Widget>[
+                                              Container(
+                                                  padding: EdgeInsets.fromLTRB(5,40,5,5),
+                                                  child:Text("Name",style: TextStyle(fontWeight: FontWeight.bold))
+                                              ),
+                                              SizedBox(height: 2.0,),
+                                              Container(
+                                                  child:Text("Decription")
+                                              ),
+                                              SizedBox(height: 2.0,),
+                                              Container(
+                                                  child:Text("Location")
+                                              )
+                                            ],
+
+                                          )
+                                        ],
+                                      )
+                                    ])
+
+
+                            )])
+                  ),
+                ],
               ),
 
             ]
